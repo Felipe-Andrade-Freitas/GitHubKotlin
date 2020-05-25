1c66d5a642144bb85089e0e4a1084c8305e40001
cachinbo <56943214+cachinbo@users.noreply.github.com>
2020-05-25 18:05:20 -0300

    tentativa de ajuste login

--- app/src/main/java/com/example/github/services/AccountService.kt
+++ app/src/main/java/com/example/github/services/AccountService.kt
@@ -1,8 +1,6 @@
 package com.example.github.services
 
-import com.example.github.models.AccessToken
 import com.example.github.models.Account
-import com.example.github.models.Repositorio
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -11,6 +9,6 @@ interface AccountService {
 
     @Multipart
     @POST("account/auth")
-    fun auth(@Body account: Account) : Call<Account>
+    fun auth(@Body account: String, toString: String) : Call<Account>
 
 }
